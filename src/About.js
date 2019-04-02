2a86e9115a3752399fcb7a8d5729c5b3a8eb441e
Sophie <sophie4ngo@gmail.com>
2019-04-02 14:01:02 -0700

    small error

--- src/About.js
+++ src/About.js
@@ -14,7 +14,7 @@ class About extends Component {
             <div className="row">
               <div className="column">
                 <img src="./aboutme.png" alt="Sophie Ngo Software Developer" />
-                  <p className="intro"> I'm financial guru, data scientist and I develop software for fun. I am based in San Francisco.</p>
+                  <p className="intro"> I'm a financial guru, data scientist and I develop software for fun. I am based in San Francisco.</p>
                   <p> I like to work with front-end frameworks such as React.js and Vue.js on my spare time. When I am not coding, 
                   I am analyzing stocks and coming with new strategies to improve my investment porfolio with the help of data science. If you want to see my code or know more about my work experience and education, click on the links below. </p>
                 <a className="logo" href="https://github.com/FunCodingPanda">
